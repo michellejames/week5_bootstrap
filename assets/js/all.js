879629cf380fa3e3c00e16b01324f94ab152575e
michellejames <shellyjames90@yahoo.com>
2017-05-07 16:56:22 -0400

    Finalizing Coffee Website

---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+
+var i = 0;
+
+i += 1;
+
+console.log("i = " + i);
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYWxsLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG52YXIgaSA9IDA7XG5cbmkgKz0gMTtcblxuY29uc29sZS5sb2coXCJpID0gXCIgKyBpKTtcbiJdfQ==
